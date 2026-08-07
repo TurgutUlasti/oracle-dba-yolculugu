99274cd98ec934f40825529828a5b3d62c644448
TurgutUlasti <110175742+TurgutUlasti@users.noreply.github.com>
2026-08-07 21:08:51 +0300

    Update and rename oracle-dba-yolculugu/Cursors_Referance.sql to oracle-dba-yolculugu/02-plsql/Cursors_Referance.sql

--- oracle-dba-yolculugu/02-plsql/Cursors_Referance.sql
+++ oracle-dba-yolculugu/02-plsql/Cursors_Referance.sql
@@ -13,7 +13,7 @@ OPEN w_imlec FOR w_query;
 LOOP
 FETCH w_imlec INTO w_eski_maas,w_yeni_maas;
 EXIT WHEN w_imlec%NOTFOUND;
-dbms_output.put_lıne('Eski Maas:'||w_eski_maas||' Zamlı Maas:'||w_yeni_maas);
+dbms_output.put_line('Eski Maas:'||w_eski_maas||' Zamlı Maas:'||w_yeni_maas);
 END LOOP;
 CLOSE w_imlec;
 END;
