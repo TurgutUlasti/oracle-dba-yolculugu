dc4a944443b8d699243f599d46e7cb6eb0077bbc
TurgutUlasti <110175742+TurgutUlasti@users.noreply.github.com>
2026-08-07 21:18:06 +0300

    Update and rename oracle-dba-yolculugu/nested1.sql to oracle-dba-yolculugu/02-plsql/nested1.sql

--- oracle-dba-yolculugu/02-plsql/nested1.sql
+++ oracle-dba-yolculugu/02-plsql/nested1.sql
@@ -8,8 +8,8 @@ puanlar ndt_puanlar;
 BEGIN
 isimler:=ndt_isimler('Turgut','Ahmet','Ayşe','Fatma','Oğuz');
 puanlar:=ndt_puanlar(98,97,78,87,92);
-dbms_output.put_lıne('Toplam Öğrenci Sayısı :'||isimler.count);
+dbms_output.put_line('Toplam Öğrenci Sayısı :'||isimler.count);
 For I IN 1..isimler.count LOOP
-dbms_output.put_lıne('Öğrenci:'||isimler(I)||' Puan:'||puanlar(I));
+dbms_output.put_line('Öğrenci:'||isimler(I)||' Puan:'||puanlar(I));
 END LOOP;
 END;
