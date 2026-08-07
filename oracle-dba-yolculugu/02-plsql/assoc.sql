928a254332ebb8212333ad02aeb07ace88371f83
TurgutUlasti <110175742+TurgutUlasti@users.noreply.github.com>
2026-08-07 21:17:37 +0300

    Update and rename oracle-dba-yolculugu/assoc.sql to oracle-dba-yolculugu/02-plsql/assoc.sql

--- oracle-dba-yolculugu/02-plsql/assoc.sql
+++ oracle-dba-yolculugu/02-plsql/assoc.sql
@@ -8,7 +8,7 @@ DECLARE
  wkare t_assoc_num;
  j     number;
  TYPE t_assoc_str IS TABLE OF employees.last_name%TYPE INDEX BY BINARY_INTEGER;
- TYPE t_assoc_str2 IS TABLE OF jobs.job_title%type INDEX BY jobs.job_ıd%type;
+ TYPE t_assoc_str2 IS TABLE OF jobs.job_title%type INDEX BY jobs.job_id%type;
  wlast_name t_assoc_str;
  wjobs t_assoc_str2;
 BEGIN
@@ -25,10 +25,10 @@ FOR I IN (SELECT last_name FROM employees WHERE ROWNUM<=10) LOOP
 
 j:=j+1;
 wlast_name(j):= I.last_name;
-dbms_output.put_lıne('wlast_name:('|| j || ') = '||wlast_name(j));
+dbms_output.put_line('wlast_name:('|| j || ') = '||wlast_name(j));
 END LOOP;
 FOR I IN(SELECT job_id,job_title FROM JOBS) LOOP
 wjobs(I.job_id):=I.job_title;
-dbms_output.put_lıne('wjobs:('|| I.job_id || ') = '||wjobs(I.job_id));
+dbms_output.put_line('wjobs:('|| I.job_id || ') = '||wjobs(I.job_id));
 END LOOP;
 END;
