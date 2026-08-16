3d2a1b23df9e3de4f32df56d3199327ee8e97e43
TurgutUlasti <110175742+TurgutUlasti@users.noreply.github.com>
2026-08-17 02:09:29 +0300

    Update Cursors2.sql

--- oracle-dba-yolculugu/02-plsql/Cursors2.sql
+++ oracle-dba-yolculugu/02-plsql/Cursors2.sql
@@ -3,35 +3,35 @@
 DECLARE
 req_emp  employees2%RowType;
 BEGIN
-UPDATE employees2 SET salary = salary * 1.20 where department_ıd=500;
+UPDATE employees2 SET salary = salary * 1.20 where department_id=500;
 
 IF SQL%FOUND THEN
-DBMS_OUTPUT.put_lıne(sql%ROWCOUNT || ' Kayıt güncellendi 1');
+DBMS_OUTPUT.put_line(sql%ROWCOUNT || ' Kayıt güncellendi 1');
 else
-DBMS_OUTPUT.put_lıne('Güncellencek Kayıt bulunamadı ! 1');
+DBMS_OUTPUT.put_line('Güncellencek Kayıt bulunamadı ! 1');
 
 END IF;
 IF SQL%NOTFOUND THEN
-DBMS_OUTPUT.put_lıne('Güncellencek Kayıt bulunamadı ! 2');
+DBMS_OUTPUT.put_line('Güncellencek Kayıt bulunamadı ! 2');
 else
-DBMS_OUTPUT.put_lıne(sql%ROWCOUNT || ' Kayıt güncellendi 2');
+DBMS_OUTPUT.put_line(sql%ROWCOUNT || ' Kayıt güncellendi 2');
 END IF;
 
-DELETE FROM employees2 where department_ıd IN (100,80);
+DELETE FROM employees2 where department_id IN (100,80);
 IF SQL%ROWCOUNT<>0 THEN 
-DBMS_OUTPUT.put_lıne('Silinecek Kayıt bulunamadı ! 1');
+DBMS_OUTPUT.put_line('Silinecek Kayıt bulunamadı ! 1');
 
 END IF;
 IF SQL%FOUND THEN
-DBMS_OUTPUT.put_lıne(sql%ROWCOUNT || ' Kayıt Silindi 1');
+DBMS_OUTPUT.put_line(sql%ROWCOUNT || ' Kayıt Silindi 1');
 else
-DBMS_OUTPUT.put_lıne('Silinecek Kayıt bulunamadı ! 1');
+DBMS_OUTPUT.put_line('Silinecek Kayıt bulunamadı ! 1');
 
 END IF;
 IF SQL%NOTFOUND THEN
-DBMS_OUTPUT.put_lıne('Silinecek Kayıt bulunamadı ! 2');
+DBMS_OUTPUT.put_line('Silinecek Kayıt bulunamadı ! 2');
 else
-DBMS_OUTPUT.put_lıne(sql%ROWCOUNT || ' Kayıt Silindi 2');
+DBMS_OUTPUT.put_line(sql%ROWCOUNT || ' Kayıt Silindi 2');
 
 
 END IF;
