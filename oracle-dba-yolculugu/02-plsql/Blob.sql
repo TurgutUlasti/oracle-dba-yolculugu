1a098e9a4f5b32dd4259c48e3b8e3e2d3f59c86f
TurgutUlasti <110175742+TurgutUlasti@users.noreply.github.com>
2026-08-17 02:13:10 +0300

    Update Blob.sql

--- oracle-dba-yolculugu/02-plsql/Blob.sql
+++ oracle-dba-yolculugu/02-plsql/Blob.sql
@@ -6,21 +6,21 @@ create table foto(
         resim    blob);
         
 DECLARE    
-foto2               bfıle;
+foto2               bfile;
 tempdata            blob;
 KaynakOffset        PLS_INTEGER:=1;
 hedefOffset         PLS_INTEGER:=1;
 dosyaIsmi           varchar2(30):='rb_35017.png';
 BEGIN 
 dbms_lob.CreateTemporary(tempdata,true);
-foto2   :=BFılename('IMAGE_DIR',dosyaIsmi);
+foto2   :=BFilename('IMAGE_DIR',dosyaIsmi);
 dbms_lob.FileOpen(foto2, dbms_lob.FILE_READONLY);
-dbms_lob.LoadFromFıle(tempdata,foto2,dbms_lob.LOBMAXSIZE,hedefOffset,KaynakOffset);
+dbms_lob.LoadFromFile(tempdata,foto2,dbms_lob.LOBMAXSIZE,hedefOffset,KaynakOffset);
 
 INSERT INTO FOTO(dosyaAdi,resim) values(dosyaIsmi,tempdata);
-commıt;
+commit;
 
-dbms_lob.FıleClose(foto2);
+dbms_lob.FileClose(foto2);
 END;
 
 --Select *From FOTO;
